ea030a9f490f2c1887293ced06c5faadb8e44896
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-05-23 15:49:59 +0300

    Create user and repo classes.

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,2 +1,13 @@
 export class User {
+    constructor(
+        public login:string,
+        public name:string,
+        public location:string,
+        public followers:number,
+        public following:number,
+        public public_repos:number,
+        public created_at:Date,
+        public avatar_url:string,
+        public html_url:string
+        ){ }
 }
