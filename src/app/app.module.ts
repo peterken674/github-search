7394278ff80f29dfff6b3dea33a8a18bdb36ccc4
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-05-23 15:33:03 +0300

    Create components and classes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ResultsComponent } from './components/results/results.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
