1b46b38328ed7d3b6276e56716faa5296e63b01b
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-05-23 15:29:31 +0300

    Add Bootstrap, Popper & Angular Material.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  access_token: "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"
 };
 
 /*
