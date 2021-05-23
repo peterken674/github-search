f1f21d943e5baf1119a004b80acb90c5d5c84aa6
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-05-23 15:54:11 +0300

    Add new components and a service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ResultsComponent } from './components/results/results.component';
 import { SearchComponent } from './components/search/search.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ProjectComponent } from './components/project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ResultsComponent,
-    SearchComponent
+    SearchComponent,
+    NavComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule
