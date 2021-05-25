dae7aaaaf723c3d33f4175cc3b776a9d6423f425
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-05-25 14:29:18 +0300

    Dry the dateCount pipe.

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -1,59 +1,34 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'dateCount'
+  name: 'dateCount',
 })
 export class DateCountPipe implements PipeTransform {
-
-    transform(value: any): any {
-        let today: any = new Date();
-        let updated: any = new Date(value);
-        var dateDifference = Math.abs(updated - today)
-        const SECONDS_IN_DAY = 86400;
-        const SECONDS_IN_HOUR = 3600;
-        const SECONDS_IN_MIN = 60;
-        var dateDifferenceSeconds = dateDifference * 0.001;
-        var dateCounter = dateDifferenceSeconds / SECONDS_IN_DAY;
-        console.log(`Updated: ${updated}, Today: ${today}`);
-
-        if (dateCounter > 30 && updated < today) {
-            var months = Math.trunc(dateCounter / 30);
-            var daysRemainder = Math.trunc(dateCounter % 30);
-            if (months > 1) {
-                var sForMonth = "s";
-            } else if (months === 1) {
-                var sForMonth = "";
+  transform(value: any, args?: any): any {
+        if (value) {
+            const timeDiffInSeconds = Math.abs((new Date().getTime() - new Date(value).getTime()) / 1000);
+            if (timeDiffInSeconds < 29)
+                return 'Just now.';
+            const times = {
+                'year': 31536000,
+                'month': 2592000,
+                'week': 604800,
+                'day': 86400,
+                'hour': 3600,
+                'minute': 60,
+                'second': 1
+            };
+            let result;
+            for (const i in times) {
+                result = Math.trunc(timeDiffInSeconds / times[i]);
+                if (result > 0)
+                    if (result === 1) {
+                        return result + ' ' + i + ' ago';
+                    } else {
+                        return result + ' ' + i + 's ago';
+                    }
             }
-            if (daysRemainder > 1) {
-                var sForDays = "s";
-            } else if (daysRemainder === 1) {
-                var sForDays = "";
-            }
-            return `${months} month${sForMonth}, ${daysRemainder} day${sForDays}`;
-        } else if (dateCounter === 30) {
-            return '1 month';
-        } else if (dateCounter < 30 && dateCounter > 1) {
-            return `${Math.trunc(dateCounter)} days`;
-        } else if (dateCounter === 1) {
-            return "1 day";
-        } else if (dateCounter < 1) {
-            var hours = Math.trunc(dateDifferenceSeconds / SECONDS_IN_HOUR);
-            if (hours > 1) {
-                return `${hours} hours`;
-            } else if (hours === 1) {
-                return '1 hour';
-            } else if (hours < 1) {
-                var minutes = Math.trunc(dateDifferenceSeconds / SECONDS_IN_MIN);
-                if (minutes > 1) {
-                    return `${minutes} minutes`;
-                } else if (minutes === 1) {
-                    return '1 minute';
-                } else if (minutes < 1) {
-                    return 'seconds';
-                }
-            }
-        }else{
-            return "seconds"
         }
+        return value;
     }
 }
